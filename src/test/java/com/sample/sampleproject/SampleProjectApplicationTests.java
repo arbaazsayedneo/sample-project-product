58a2dcf4fb198563c7711e696c0c9a9b1ec336c5
arbaazsayedneo <138784466+arbaazsayedneo@users.noreply.github.com>
2024-01-09 19:52:38 +0530

    test

--- src/test/java/com/sample/sampleproject/SampleProjectApplicationTests.java
+++ src/test/java/com/sample/sampleproject/SampleProjectApplicationTests.java
@@ -1,13 +1,9 @@
 package com.sample.sampleproject;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SampleProjectApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
